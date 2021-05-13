001221e7ad5afb62470a2d8c5b4458c17be88a21
a.wink <a.wink@kellerkinder.de>
2021-05-13 11:35:48 +0200

    Add composer and brew installation

--- init-mac.sh
+++ init-mac.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#install brew (needs password)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# package installation
 bash ./mac/init-brew.sh
 bash ./mac/init-npm.sh
 cp -r home/* ~/
+# install composer
+curl https://getcomposer.org/download/2.0.13/composer.phar -o /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
