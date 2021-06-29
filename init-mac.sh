947d75d72d6ced8238e980d0d5d847d59a581d0e
a.wink <a.wink@kellerkinder.de>
2021-06-29 14:53:51 +0200

    - add fswatch - add symfony binary

--- init-mac.sh
+++ init-mac.sh
@@ -4,7 +4,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # update pip
 /usr/local/opt/python@3.9/bin/python3.9 -m pip install --upgrade pip
-
+curl -sS https://get.symfony.com/cli/installer | bash
+mv ~/.symfony/bin/symfony /usr/local/bin/
 
 # package installation
 bash ./mac/init-brew.sh
