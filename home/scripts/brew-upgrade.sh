23579e3ea149390e83e7aa847690324116845968
Alexander Wink <alexander.wink@dasistweb.de>
2024-04-19 14:13:34 +0200

    fix: Add update handling to exclude phpstorm

--- home/scripts/brew-upgrade.sh
+++ home/scripts/brew-upgrade.sh
@@ -1,2 +1,3 @@
 brew update -q && brew upgrade -q
-brew upgrade -q --casks $(brew list --casks)
+#brew upgrade -q --casks $(brew list --casks)
+brew upgrade -q --casks $(brew list --casks -1 | grep --invert-match phpstorm) # exclude phpstorm from update due to latest changes
